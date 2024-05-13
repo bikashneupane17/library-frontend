15ed922011a5d70b52802f386c29d45bd0666f9b
bikashneupane17 <edubikkash@gmail.com>
2024-05-13 13:45:35 +1000

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Button } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/login-signup/Signup";
+import { Login } from "./pages/login-signup/Login";
+import { HomePage } from "./pages/home/HomePage";
 
 function App() {
-  return <>Frontend</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
