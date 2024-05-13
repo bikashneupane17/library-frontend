dc0ba955fd4d2b675305e3ff279b969f0636f30e
bikashneupane17 <edubikkash@gmail.com>
2024-05-13 14:59:51 +1000

    Card,Carousel added

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import { DefaultLayout } from "../../components/layout/DefaultLayout";
+import { CustomCarousel } from "../../components/carousel/CustomCarousel";
+import { Container, Row, Col, Form } from "react-bootstrap";
+import { CustomCard } from "../../components/card/CustomCard";
 
 export const HomePage = () => {
-  return <DefaultLayout>Home</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <CustomCarousel />
+
+      {/* Book List */}
+      <Container>
+        <Row>
+          <Col className="mt-2 d-flex justify-content-between">
+            <label htmlFor="">20 Books Found!</label>
+            <div className="">
+              <Form.Control placeholder="Search by name..." />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <CustomCard />
+          </Col>
+        </Row>
+      </Container>
+    </DefaultLayout>
+  );
 };
